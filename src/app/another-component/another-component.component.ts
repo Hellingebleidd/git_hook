7e34ee78755e74a02e09e559303f0c757ae1d0dc
Zuzana Nadzamova <zuzana.nadzamova@posam.sk>
2024-06-07 10:38:19 +0200

    [PARKDOTS-5270] added another component

---
+++ src/app/another-component/another-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-another-component',
+  templateUrl: './another-component.component.html',
+  styles: [
+  ]
+})
+export class AnotherComponentComponent {
+
+}
