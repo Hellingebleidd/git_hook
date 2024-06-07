2d849cd1956f84545371fe2232acb119ab0166c8
Zuzana Nadzamova <zuzana.nadzamova@posam.sk>
2024-06-07 12:02:21 +0200

    [PARKDOTS-1234] deleted scss file

--- src/app/new-page/new-page.component.ts
+++ src/app/new-page/new-page.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
-  styleUrls: ['./new-page.component.scss']
 })
 export class NewPageComponent {
 
